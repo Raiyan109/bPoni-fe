f12ec98ae8b2b3be586dd07bed392d9c32912ed7
Raiyan109 <kraiyan109@gmail.com>
2024-01-28 07:19:23 +0600

    sidebar responsive

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,16 +6,16 @@ const Menu = () => {
     return (
         <div className='menu'>
             <div className="item flex flex-col gap-3 mb-3">
-                <span className="title text-xl font-bold">Main</span>
+                <span className="title text-xl font-bold hidden lg:block">Main</span>
                 <Link to='/' className='flex items-center gap-2'>
-                    <span className="listItemTitle">Delivery Unavailable</span>
+                    <span className="listItemTitle hidden lg:block">Delivery Unavailable</span>
                 </Link>
             </div>
             <div className="item flex flex-col gap-3 mb-3">
-                <span className="title text-xl font-bold">Catalog</span>
-                <Link to='/' className='flex items-center gap-2 hover:bg-gray-200 rounded-md px-2 py-3'>
+                <span className="title text-xl font-bold hidden lg:block">Catalog</span>
+                <Link to='/' className='flex items-center gap-2 hover:bg-gray-200 rounded-md px-1 lg:px-2 py-3'>
                     <img src={donut} alt="" className='w-10 h-10 object-contain rounded-full' />
-                    <span className="listItemTitle text-lg font-medium">Home</span>
+                    <span className="listItemTitle text-lg font-medium hidden lg:block">Home</span>
                 </Link>
             </div>
 
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
         <div className="main">
             <Navbar />
             <div className="container flex">
-                <div className="menuContainer w-[480px] py-1 px-7 border-r border-gray-500">
+                <div className="menuContainer w-auto lg:w-[480px] py-1 px-3 lg:px-7 border-r border-gray-500">
                     <Menu />
                 </div>
                 <div className="contentContainer py-1 px-7 w-full">
